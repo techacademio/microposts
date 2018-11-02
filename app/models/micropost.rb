b626148ef7d9c6832d319023be92935f4950cf0f
tech.academio <techacademio@gmail.com>
2018-11-02 20:22:34 +0000

    like

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :reverses_of_favorite, class_name: 'Favorite'
+  
   validates :user_id, presence:true
   validates :content, presence:true, length: { maximum: 255 }
 end
---
+++ db/migrate/20181102144310_rename_like_to_favorite.rb
@@ -0,0 +1,4 @@
+class RenameLikeToFavorite < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
